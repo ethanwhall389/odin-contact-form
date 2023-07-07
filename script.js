b25bd71b94b51d6aa547f931832c49fcdab8cecb
Ethan Hall <ethanwhall@icloud.com>
2023-07-06 21:26:36 -0400

    Commit things missed last commit

--- script.js
+++ script.js
@@ -1,8 +1,6 @@
 let pwd01 = document.querySelector('input#pwd')
 let pwd02 = document.querySelector('input#confirm-pwd')
-let pwdContainer = document.querySelector('.pwd-container');
-let message = document.createElement('p');
-pwdContainer.appendChild(message);
+let message = document.querySelector('.match-error');
 
 function comparePwd () {
     if (pwd01.value !== pwd02.value) {
@@ -11,11 +9,14 @@ function comparePwd () {
         message.textContent = '';
     }
 }
+pwd02.addEventListener('keyup', () => comparePwd())
 
+pwd01.addEventListener('keyup', () => {
+    comparePwd();
+    checkVisibility();
+})
 
-pwd02.addEventListener('keyup', () => comparePwd());
-
-pwd01.addEventListener('keyup', () => comparePwd());
+// Show password feature
 
 
 let showPwd = document.querySelector('input#show-pwd')
@@ -25,7 +26,24 @@ showPwd.addEventListener('click', () => CheckShowPwd())
 function CheckShowPwd () {
     if (showPwd.checked == true) {
         pwd01.setAttribute('type', 'text');
+        pwd02.setAttribute('type', 'text');
     } else {
         pwd01.setAttribute('type', 'password');
+        pwd02.setAttribute('type', 'password');
+    }
+}
+
+// Only show the show pwd option if user is typing in pwd field.
+
+let showPwdContainer = document.querySelector('.view-pwd-container');
+
+function checkVisibility () {
+    console.log('test');
+    if (pwd01.value == '') {
+        showPwdContainer.classList.remove('show-pwd');
+        showPwdContainer.classList.add('hide-pwd');
+    } else {
+        showPwdContainer.classList.remove('hide-pwd');
+        showPwdContainer.classList.add('show-pwd');
     }
 }
